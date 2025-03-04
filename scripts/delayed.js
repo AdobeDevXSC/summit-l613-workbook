8405120ff37f3f914dd1c546079ef85d36159858
Lamont Crook <lamont@adobe.com>
2025-03-04 16:59:55 -0700

    update styling

--- scripts/delayed.js
+++ scripts/delayed.js
@@ -1,1 +1,11 @@
 // add delayed functionality here
+const element = document.querySelector('.navigation');
+const offset = element.offsetTop;
+
+window.addEventListener('scroll', () => {
+  if (window.pageYOffset >= offset) {
+    element.classList.add('stuck');
+  } else {
+      element.classList.remove('stuck');
+  }
+});
